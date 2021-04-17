7be01b6971e3ea3bc92b5fd97cca049978486765
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-04-16 22:47:23 -0300

    Atualização do arquivo do BD
    Correção do arquivo do Banco de Dados do projeto, com a criação de duas tabelas Leito e Paciente, com suas respectivas correções

--- BD/hospital_universitario.sql
+++ BD/hospital_universitario.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS hospital_universitario DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE hospital_universitario;
+CREATE DATABASE IF NOT EXISTS hospital_universitario2 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE hospital_universitario2;
 
 /*Cria tabela de admninistradores*/
 CREATE TABLE administradores ( 
@@ -13,20 +13,20 @@ CREATE TABLE agendamento (
   Data_Agend date NOT NULL,
   Realizado tinyint(1) DEFAULT NULL,
   Hora_Agend time NOT NULL,
-  CPF_prontuario char(11) NOT NULL,
+  ID_prontuario bigint(20) NOT NULL,
   CPF_usuario char(11) , /* Cahve estrangeira que refencia o Responsavel*/
   Cod_medicamento bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Cria tabela do relacionamento agendamento<->prontuario*/
 CREATE TABLE agendamento_prontuario (
-  CPF_Prontuario char(11) DEFAULT NULL,
+  ID_prontuario bigint(20) DEFAULT NULL,
   Codigo_Agendamento bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Cria tabela da CID*/
 CREATE TABLE cid (
-  CPF_prontuario char(11) NOT NULL,
+  ID_prontuario bigint(20) NOT NULL,
   CodCID char(6) NOT NULL,
   Id bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -72,22 +72,34 @@ CREATE TABLE medicamento (
 CREATE TABLE ocorrencia (
   Codigo bigint(20) NOT NULL,
   Data_ocorr date NOT NULL,
-  CPF_prontuario char(11) NOT NULL,
+  ID_prontuario bigint(20) NOT NULL,
   Descricao text NOT NULL,
   CPF char(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Cria tabela de prontuarios*/
 CREATE TABLE prontuario (
+  ID bigint(20) NOT NULL,  
+  Data_Internacao date NOT NULL,
+  Data_Saida date NOT NULL,
+  Status enum('internado','alta','obito') NOT NULL,  
+  Id_leito varchar(20) NOT NULL,
+  Cpfpaciente char(11)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Cria tabela de prontuarios*/
+CREATE TABLE paciente (
   Nome_Paciente varchar(50) NOT NULL,
   Sexo varchar(20) NOT NULL,
   Data_Nasc date NOT NULL,
   CPF char(11) NOT NULL,
-  Tipo_Sang varchar(5) NOT NULL,
-  Data_Internacao date NOT NULL,
-  Data_Saida date NOT NULL,
-  Leito varchar(10) NOT NULL,
-  Status enum('internado','alta','obito') NOT NULL
+  Tipo_Sang varchar(5) NOT NULL  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Cria tabela de prontuarios*/
+CREATE TABLE leito (
+  Ocupado tinyint(1) NOT NULL,
+  Identificacao varchar(20) NOT NULL    
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Cria tabela de administradores*/
@@ -113,17 +125,17 @@ ALTER TABLE administradores
 
 ALTER TABLE agendamento
   ADD PRIMARY KEY (Codigo),
-  ADD KEY CPF_prontuario (CPF_prontuario),
+  ADD KEY ID_prontuario (ID_prontuario),
   ADD KEY CPF_usuario (CPF_usuario),
   ADD KEY Cod_medicamento (Cod_medicamento);
 
 ALTER TABLE agendamento_prontuario
-  ADD KEY CPF_Prontuario (CPF_Prontuario),
+  ADD KEY ID_prontuario (ID_prontuario),
   ADD KEY Codigo_Agendamento (Codigo_Agendamento);
 
 ALTER TABLE cid
   ADD PRIMARY KEY (Id),
-  ADD KEY CPF_prontuario (CPF_prontuario);
+  ADD KEY ID_prontuario (ID_prontuario);
 
 ALTER TABLE enfermeiros
   ADD PRIMARY KEY (COREN),
@@ -144,17 +156,27 @@ ALTER TABLE medicamento
 
 ALTER TABLE ocorrencia
   ADD PRIMARY KEY (Codigo),
-  ADD KEY CPF_prontuario (CPF_prontuario),
+  ADD KEY ID_prontuario (ID_prontuario),
   ADD KEY CPF (CPF);
 
 ALTER TABLE prontuario
-  ADD PRIMARY KEY (CPF);
+  ADD PRIMARY KEY (ID),
+  ADD KEY Id_leito (Id_leito),
+  ADD KEY Cpfpaciente (Cpfpaciente);
 
 ALTER TABLE responsaveis
   ADD PRIMARY KEY (CPF);
 
 ALTER TABLE usuarios
   ADD PRIMARY KEY (CPF);
+  
+ALTER TABLE paciente
+  ADD PRIMARY KEY (CPF);
+
+ALTER TABLE leito
+  ADD PRIMARY KEY (Identificacao);
+  
+  
 
 
 ALTER TABLE cid
@@ -165,16 +187,16 @@ ALTER TABLE administradores
   ADD CONSTRAINT administradores_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF);
 
 ALTER TABLE agendamento
-  ADD CONSTRAINT agendamento_ibfk_1 FOREIGN KEY (CPF_prontuario) REFERENCES prontuario (CPF),
+  ADD CONSTRAINT agendamento_ibfk_1 FOREIGN KEY (ID_prontuario) REFERENCES prontuario (ID),
   ADD CONSTRAINT agendamento_ibfk_2 FOREIGN KEY (CPF_usuario) REFERENCES usuarios (CPF),
   ADD CONSTRAINT agendamento_ibfk_3 FOREIGN KEY (Cod_medicamento) REFERENCES medicamento (Codigo);
 
 ALTER TABLE agendamento_prontuario
-  ADD CONSTRAINT agendamento_prontuario_ibfk_1 FOREIGN KEY (CPF_Prontuario) REFERENCES prontuario (CPF),
+  ADD CONSTRAINT agendamento_prontuario_ibfk_1 FOREIGN KEY (ID_prontuario) REFERENCES prontuario (ID),
   ADD CONSTRAINT agendamento_prontuario_ibfk_2 FOREIGN KEY (Codigo_Agendamento) REFERENCES agendamento (Codigo);
 
 ALTER TABLE cid
-  ADD CONSTRAINT cid_ibfk_1 FOREIGN KEY (CPF_prontuario) REFERENCES prontuario (CPF);
+  ADD CONSTRAINT cid_ibfk_1 FOREIGN KEY (ID_prontuario) REFERENCES prontuario (ID);
 
 ALTER TABLE enfermeiros
   ADD CONSTRAINT enfermeiros_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF);
@@ -186,8 +208,12 @@ ALTER TABLE estagiarios
   ADD CONSTRAINT estagiarios_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF);
 
 ALTER TABLE ocorrencia
-  ADD CONSTRAINT ocorrencia_ibfk_1 FOREIGN KEY (CPF_prontuario) REFERENCES prontuario (CPF),
+  ADD CONSTRAINT ocorrencia_ibfk_1 FOREIGN KEY (ID_prontuario) REFERENCES prontuario (ID),
   ADD CONSTRAINT ocorrencia_ibfk_2 FOREIGN KEY (CPF) REFERENCES usuarios (CPF);
 
 ALTER TABLE responsaveis
   ADD CONSTRAINT responsaveis_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF);
+  
+ALTER TABLE prontuario
+  ADD CONSTRAINT prontuario_ibfk_1 FOREIGN KEY (Id_leito) REFERENCES leito (Identificacao),
+  ADD CONSTRAINT prontuario_ibfk_2 FOREIGN KEY (Cpfpaciente) REFERENCES paciente (CPF);
