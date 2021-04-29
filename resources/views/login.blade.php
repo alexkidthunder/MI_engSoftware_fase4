81bc2f3e4667ab4839f05dff0b15b041aef86dab
Estéfane <53922973+tefscs@users.noreply.github.com>
2021-04-29 17:05:11 -0300

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>NOME DO NOSSO </title>
+     
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
     <link href="{{ ('css/login-style.css') }}" rel="stylesheet"> 
 
 </head>
-<body>
+<body class="body-login">
     <div class="container">
         <div class="row">
             <div class="col-lg">
@@ -32,31 +32,23 @@
         </div>
     </div>
    
-    <form class = "form" action="/index/menu" method= "post">
+    <form id="login" action="/index/menu" method= "post">
     @csrf
-    <div class="card">
-        <div class="card-top">
-            <h2 class = titulo>Login</h2>
-        </div>
-            
-            <div class="card-groupp">
+    <div class="box-login">
+            <h2>Login</h2>
+            <div>
                 <label>CPF</label>
                 <input type="text" name= "cpf" placeholder="Digite seu CPF" required maxlength="14" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}">
             </div>
-           
-            <div class="card-groupp">
+            <div>
                 <label>Senha</label>
                 <input type="password" name= "senha" placeholder="Digite sua senha" required>
             </div>
-
-            <div class="card-groupp btn">
+            <div class="enter">
                 <button type= "submit">ENTRAR</button>
             </div>
 
-            <div class="texto-interativo">
-                <label>Esqueceu a senha?</label>
-            </div>
-
+            <label class="text">Esqueceu a senha?</label>
         </div>
     </form>
     <!-- JavaScript Bundle with Popper -->
