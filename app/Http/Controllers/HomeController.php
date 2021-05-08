00c952ff34b7ba446a9910d6ce448f56957ff2a6
WesleyRdS <wesleysrw@gmail.com>
2021-05-08 01:36:21 -0300

    Função de login atualizada e função de atribuição implementadaa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,32 +11,46 @@ class HomeController extends Controller
     }
 
     public function login(Request $request){
+        include("db.php");
         $request -> validate([
             'cpf' => 'required',
             'senha' => 'required'
         ]);
-        $users = DB::table('usuarios')->where('CPF',$request->cpf)->first();;
-        if($users != null){
-            if($users->Senha == $request->senha){
-                if($users->Atribuicao == "Administrador"){
-                    return redirect() -> intended('menu');
-                }else if($users->Atribuicao == "Enfermeiro Chefe"){
-                    return redirect() -> intended('menuEnfermeiroChefe');
-                }else if($users->Atribuicao == "Enfermeiro"){
-                    return redirect() -> intended('menuEnfermeiro');
-                }else if($users->Atribuicao == "Estagiario"){
-                    return redirect() -> intended('menuEstagiario');
-                }else{
-                    return redirect() -> back() ->with('msg','Funcionario sem cargo, algo esta errado!!!');
-                }
-                
+        $result = mysqli_query($connect,"SELECT CPF FROM usuarios WHERE CPF = '$request->cpf' AND Senha = '$request->senha'"); /*Verificando se cpf e senha estão cadastrados no banco de dados*/ 
+        $row = mysqli_num_rows($result); /*resultado da verificalçao*/
+        /*While percorrendo vetor gerado pela query */
+        $sql = "SELECT * FROM usuarios where CPF = '$request->cpf'";
+        $query = mysqli_query($connect,$sql);
+        while($sql = mysqli_fetch_array($query)){
+            $atribuicao = $sql["Atribuicao"];
+        }
+        if($row == 1){ // verifica se o usuario existe no sistema. $row = 1 significa que sim
+            $_SESSION['usuario'] = $request->cpf; // inicia uma sessão de nome usuario com o cpf recuperado
+            
+            /*Sequência de condicionais que verifica o cargo para reirecionar para o menu correto */
+            if($atribuicao == "Administrador"){
+                return redirect() -> intended('menu');
+            }else if($atribuicao == "Enfermeiro Chefe"){
+                return redirect() -> intended('menuEnfermeiroChefe');
+            }else if($atribuicao == "Enfermeiro"){
+                return redirect() -> intended('menuEnfermeiro');
+            }else if($atribuicao == "Estagiario"){
+                return redirect() -> intended('menuEstagiario');
             }else{
-                return redirect() -> back() ->with('msg','Acesso negado para essas credenciais');
+                return redirect() -> back() ->with('msg','Funcionario sem cargo, algo esta errado!!!');
             }
-        }else{
-            return redirect() -> back() ->with('msg','Usuario não cadastrado');
+        }else{ // caso em que o $row = 0
+            return redirect() -> back() ->with('msg','Acesso negado para essas credenciais');
         }
 
+    }
+
+    public function verificarLoguin(){
+        include('db.php');
+        session_start();
+        if(!$_SESSION['usuario']){
+            return view('../login.php');
+        }
     }
 
     public function logout(){
@@ -49,22 +63,6 @@ class HomeController extends Controller
         return view('primeiroAcesso');
     }
 
-    public function sessaoAdmin(){
-        session_start();
-        return view('admin.menu');
-    }
-    public function sessaoEnf(){
-        session_start();
-        return view('enf.menu');
-    }
-    public function sessaoEnfChef(){
-        session_start();
-        return view('enfChefe.menu');
-    }
-    public function sessaoEst(){
-        session_start();
-        return view('estagiario.menu');
-    }
 
     public function menu(){
         return view('admin.menu');
