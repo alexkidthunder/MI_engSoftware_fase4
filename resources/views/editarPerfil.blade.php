8d2093feb99d5b2611174c5a9cab852f882d7d4f
Estéfane <53922973+tefscs@users.noreply.github.com>
2021-05-09 17:59:53 -0300

    alterar senha

--- resources/views/editarPerfil.blade.php
+++ resources/views/editarPerfil.blade.php
@@ -11,8 +11,8 @@
 
     <!-- Favicon -->
     <link href="{{ asset('img/favicon.png') }}" rel="icon">
-    
-    <script src="{{ ('js/editarPerfil.js') }}" defer></script>
+
+    <script src="{{ 'js/editarPerfil.js' }}" defer></script>
 
     <title>Meu perfil</title>
 </head>
@@ -49,13 +49,15 @@
                                 <div class="row no-gutters">
                                     <div id="fmasc_div" class="col-lg-6 border-m content-center">
                                         <input type="radio" name="fsexo" value="Masculino" id="fmasc" disabled>
-                                            <label for="fmasc" class="normal-label">Masculino</label>
-                                        </input> <!--Usar o checked para deixar marcado-->
+                                        <label for="fmasc" class="normal-label">Masculino</label>
+                                        </input>
+                                        <!--Usar o checked para deixar marcado-->
                                     </div>
                                     <div id="ffem_div" class="col-lg-6 border-f content-center">
                                         <input type="radio" name="fsexo" value="Feminino" id="ffem" disabled>
-                                            <label for="ffem" class="normal-label">Feminino</label>
-                                        </input> <!--Usar o checked para deixar marcado-->
+                                        <label for="ffem" class="normal-label">Feminino</label>
+                                        </input>
+                                        <!--Usar o checked para deixar marcado-->
                                     </div>
                                 </div>
                             </div>
@@ -68,54 +70,67 @@
                             </div>
                             <div class="col-lg-4" id="atribuiDiv">
                                 <label for="fatribui">Atribuição</label>
-                                <input disabled id="fatribui" name="fatribui" type="text" value="Enfermeiro"> <!--Alterar o Value de acordo com a atribuição-->
+                                <input disabled id="fatribui" name="fatribui" type="text" value="Enfermeiro">
+                                <!--Alterar o Value de acordo com a atribuição-->
                             </div>
                         </div>
-                        <div class="row"> 
-                            <div class="col-lg-4" id="corenDiv" style="display: none;"> <!--Mostrar isso somente se for Enfermeiro/Chefe-->
+                        <div class="row">
+                            <div class="col-lg-4" id="corenDiv" style="display: none;">
+                                <!--Mostrar isso somente se for Enfermeiro/Chefe-->
                                 <label for="fcoren">Coren</label>
-                                <input disabled id="fcoren" name="fcoren" type="text" value="Coren do Usuario" required maxlength="9">
+                                <input disabled id="fcoren" name="fcoren" type="text" value="Coren do Usuario" required
+                                    maxlength="9">
                             </div>
                             <div class="col-lg-4" id="edit_div">
                                 <br>
                                 <div id="psw_info_div" style="display: none;">
-                                    <button type="button" class="btn-white" name="psw_info" id="psw_info">Alterar senha</button>
+                                    <button type="button" class="btn-white" name="psw_info" id="psw_info">Alterar
+                                        senha</button>
                                 </div>
                             </div>
                         </div>
                         <div id="edit_div" class="row">
                             <div class="col-lg" id="edit_info_div">
-                                <button type="button" class="btn-blue" name="edit_info" id="edit_info">Editar informações</button>
+                                <button type="button" class="btn-blue" name="edit_info" id="edit_info">Editar
+                                    informações</button>
                             </div>
                             <div class="col-lg" id="confirm_info_div" style="display: none;">
-                               <button type="button" class="btn-blue" name="confirm_info" id="confirm_info">Salvar</button> <!--Alterar para Submit depois-->
+                                <button type="button" class="btn-blue" name="confirm_info"
+                                    id="confirm_info">Salvar</button>
+                                <!--Alterar para Submit depois-->
                             </div>
                         </div>
                     </form>
-                    <div id="psw" style="display: none">
-                    <h3 class="text-center">Alterar senha</h3> <br>
-                        <form class="content-center">
-                                <div align="center">
-                                    <input type="password" id="senha-atual" name="senha-atual" placeholder="insira a senha atual" required>
-                                </div>
-                                <div align="center">
-                                    <input type="password" id="senha" name="senha" placeholder="insira a nova senha" required>
-                                </div>
-                                <div align="center">
-                                    <input type="password" id="confirmacao" name="confirmacao" placeholder="Confirme a nova senha" required>
-                                </div>
-                                <br>
-                                <br>
-                            <div class="row" align="right">
-                                <div align="right">
+                </div>
+            </div>
+            <div class="set-password">
+                <div id="psw" style="display: none">
+                    <h3 class="password-title text-center">Alterar senha</h3>
+                    <br> <br>
+                    <form class="content-center">
+                        <div align="center">
+                            <input type="password" id="senha-atual" name="senha-atual"
+                                placeholder="insira a senha atual" required>
+                        </div>
+                        <div align="center">
+                            <input type="password" id="senha" name="senha" placeholder="insira a nova senha" required>
+                        </div>
+                        <div align="center">
+                            <input type="password" id="confirmacao" name="confirmacao"
+                                placeholder="Confirme a nova senha" required>
+                        </div>
+                        <br>
+                        <div class="content-right">
+                            <div class="row">
+                                <div class="col-lg">
                                     <button class="btn-gray">Cancelar</button>
                                 </div>
-                                <div align="right">
-                                    <button type="submit" class="btn-blue">Alterar senha</button>  
+                                <div class="col-lg">
+                                    <button type="submit" class="btn-blue">Alterar senha</button>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
