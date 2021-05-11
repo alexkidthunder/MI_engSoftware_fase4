adcab92ce1371df55ad2962bd17275c26943cc52
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2021-05-11 13:35:01 -0300

    Update Hide

--- public/js/editarPerfil.js
+++ public/js/editarPerfil.js
@@ -1,144 +1,157 @@
-/*$('.edit').click(function(){
-
-    $('.change').html(
-        '<div class="change">'+
-            '<form id="register">'+
-                '<div class="row">'+
-                    '<div class="col-lg-12">'+
-                        '<label>Nome</label> <br>'+
-                        '<input id="fnome" name="fnome" type="text" maxlength="50" required>'+
-                    '</div>'+
-                '</div>'+
-
-                '<div class="row">'+
-                    '<div class="col-lg-4">'+
-                        '<label>Data de Nascimento</label> <br>'+
-                        '<input id="fnascimento" name="fnascimento" type="date" required>'+
-                    '</div>'+
-                    '<div class="col-lg-4">'+
-                        '<label>CPF</label> <br>'+
-                        '<input id="fcpf" name="fcpf" type="text" required maxlength="14" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}">'+
-                        '</div>'+
-                        '<div class="col-lg-4">'+
-                            '<div class="sex-form">'+
-                                '<label>Sexo</label> <br>'+
-                                '<input id="MASCULINO" name="fsexo" value="Masculino" type="button">'+
-                                '<input id="FEMININO" name="fsexo" value="Feminino" type="button">'+
-                            '</div>'+
-                        '</div>'+
-                    '</div>'+
-
-                    '<div class="row">'+
-                        '<div class="col-lg-8">'+
-                            '<label>Email</label> <br>'+
-                            '<input id="femail" name="femail" type="email" maxlength="50" required>'+
-                        '</div>'+
-                    '</div>'+
-                    '</form>'+
-                    '</div>'
-                        
-    );
-}) */
-
+/* -- Campos com as informações do usuário -- */
 var nomeInp = document.getElementById('fnome');
-var fmInp = document.getElementById('fmasc');
-var ffInp = document.getElementById('ffem');
+var mascInp = document.getElementById('fmasc');
+var femInp = document.getElementById('ffem');
+var radioSexBtn = document.getElementsByName('fsexo');
 var emailInp = document.getElementById('femail');
+var corenDiv =  document.getElementById('corenDiv');
+var atribuiInp = document.getElementById('fatribui');
 
-var confBtnDiv = document.getElementById('confirm_info_div');
+/* -- Div que englobam os botões -- */
+var saveBtnDiv = document.getElementById('confirm_info_div');
 var editBtnDiv = document.getElementById('edit_info_div');
-var passBtnDiv = document.getElementById('psw_info_div');
+var pswBtnDiv = document.getElementById('psw_info_div');
 
-var pswArea = document.getElementById('psw');
-var pflArea = document.getElementById('register');
+/* -- As areas de dados do usuário (perfil) e a parte de trcar a senha -- */
+var pswArea = document.getElementById('pswArea');
+var pflArea = document.getElementById('perfilArea');
 
+/* -- Div com os radio button do sexo -- */
+var radioMascDiv = document.getElementById('fmasc_div');
+var radioFemDiv = document.getElementById('ffem_div');
+
+/* -- Botões presentes na tela de perfil -- */
 var editBnt = document.getElementById('edit_info');
-editBnt.addEventListener("click",function(){
-    editBtnDiv.style.display = "none";
-    confBtnDiv.style.display = "";
-    passBtnDiv.style.display = "";
+var saveBtn = document.getElementById('confirm_info');
+var pswBtn = document.getElementById('psw_info');
 
-    
-    radioFemDiv.style.display = "";
-    radioMascDiv.style.display = "";
+/* -- Botões presentes na tela de alterar senha -- */
+var cancelarBtn = document.getElementById('cancelar');
+var alterarBtn = document.getElementById('alterarSenha');
+
+/* -- Inputs presentes na tela de alterar senha -- */
+var SenhaAtual = document.getElementById('senha-atual');
+var SenhaNova = document.getElementById('senha');
+var SenhaNovaConfirmacao = document.getElementById('confirmacao');
+
+/* -- Verifica a atribuição e mostra, ou não, a informação do coren -- */
+if(atribuiInp.value === "Enfermeiro" || atribuiInp.value === "Enfermeiro Chefe"){
+    corenDiv.classList.remove('hide');
+}
+else{
+    corenDiv.classList.add('hide');
+}
+
+/* -- Verifica se o sexo já foi escolhido, caso não, mostra os botões para escolher -- */
+if(femInp.checked === false && mascInp.checked === false){
+    addNoRadialEdit();
+}
+else{
+    checkRadial();
+}
+
+/* -- ao clicar no botão "Editar Informações" -- */
+editBnt.addEventListener("click",function(){
 
     enable();   
     removeNoRadialEdit();
     addRadialEdit();
+    setSexRequired();
+    setPerfilDataRequired();
+    toggleHideBtnPerfil();
+    refreshSex();
+    checkRadial();
 
 })
 
-var confBtn = document.getElementById('confirm_info');
-confBtn.addEventListener("click",function(){
-    editBtnDiv.style.display = "";
-    confBtnDiv.style.display = "none";
-    passBtnDiv.style.display = "none";
+/* -- ao clicar no botão "Salvar" -- */
+saveBtn.addEventListener("click",function(){
 
     disable();
     addNoRadialEdit();
     removeRadialEdit();
     checkRadial();
-    
-})
-
-
-var pswInp = document.getElementById('psw_info');
-pswInp.addEventListener("click",function(){
-
-    pflArea.style.display = "none";
-    pswArea.style.display = "";
+    removePerfilDataRequired();
+    removeSexRequired();
+    toggleHideBtnPerfil();
 
 })
 
 
-var backBtn = document.getElementById('fback');
-backBtn.addEventListener("click",function(){
+pswBtn.addEventListener("click",function(){
 
-    editBtnDiv.style.display = "";
-    confBtnDiv.style.display = "none";
-    passBtnDiv.style.display = "none";
+    removePerfilDataRequired();
+    removeSexRequired();
+    setPswRequired();
+    pflArea.classList.add('hide');
+    pswArea.classList.remove('hide');
 
-    pflArea.style.display = "";
-    pswArea.style.display = "none";
+})
+
+cancelarBtn.addEventListener("click",function(){
 
     disable();
-
-    ffInp.checked = false;
-    fmInp.checked = false;
-
-    removeRadialEdit();
     addNoRadialEdit();
+    removeRadialEdit();
+    pflArea.classList.remove('hide');
+    pswArea.classList.add('hide');
+    setInitialState();
     checkRadial();
-
+    removePswRequired();
 
 })
 
-var atribuiInp = document.getElementById('fatribui');
-var corenDiv =  document.getElementById('corenDiv');
-console.log("Chegou aqui");
-if(atribuiInp.value === "Enfermeiro" || atribuiInp.value === "Enfermeiro Chefe"){
-    corenDiv.style.display = "";
-    console.log("Chegou na parte do coren");
+function setPswRequired(){
+    SenhaAtual.setAttribute("required","");
+    SenhaNova.setAttribute("required","")
+    SenhaNovaConfirmacao.setAttribute("required","");
 }
-else{
-    corenDiv.style.display = "none";
-    console.log("Passou reto");
+function removePswRequired(){
+    SenhaAtual.removeAttribute("required");
+    SenhaNova.removeAttribute("required");
+    SenhaNovaConfirmacao.removeAttribute("required");
+}
+function setInitialState(){
+    if(editBtnDiv.classList.contains('hide')){
+        editBtnDiv.classList.remove('hide');
+        saveBtnDiv.classList.add('hide');
     }
-
-
-var radioMascDiv = document.getElementById('fmasc_div');
-var radioFemDiv = document.getElementById('ffem_div');
-
-// fmInp = Input radial masculino
-// ffInp = Input radial feminino
-
-if(ffInp.checked === false && fmInp.checked === false){
-    addNoRadialEdit();
 }
-else{
-    checkRadial();
+function toggleHideBtnPerfil(){
+    if(saveBtnDiv.classList.contains('hide')){
+        editBtnDiv.classList.add('hide');
+        saveBtnDiv.classList.remove('hide');
+    }
+    else if(editBtnDiv.classList.contains('hide')){
+        editBtnDiv.classList.remove('hide');
+        saveBtnDiv.classList.add('hide');
+    }
 }
-
+function setPerfilDataRequired(){
+    nomeInp.setAttribute("required","");
+    emailInp.setAttribute("required","");
+}
+function removePerfilDataRequired(){
+    nomeInp.removeAttribute("required");
+    emailInp.removeAttribute("required");
+}
+function setSexRequired(){
+    radioSexBtn.forEach(element => {
+        element.setAttribute("required","");
+    });
+}
+function removeSexRequired(){
+    radioSexBtn.forEach(element => {
+        element.removeAttribute("required");
+    });
+}
+function refreshSex(){
+    radioSexBtn.forEach(element => {
+        if(element.checked){
+            element.checked = false; 
+        }
+    })
+}
 function addNoRadialEdit(){
     radioFemDiv.classList.add("radial-no-edit");
     radioMascDiv.classList.add("radial-no-edit");
@@ -157,24 +170,28 @@ function removeRadialEdit(){
 }
 
 function checkRadial(){
-    if(ffInp.checked === true){
-        radioMascDiv.style.display = "none";
+    if(femInp.checked === true){
+        radioMascDiv.classList.add('hide');
     }
-    else if(fmInp.checked === true){
-        radioFemDiv.style.display = "none";
+    else if(mascInp.checked === true){
+        radioFemDiv.classList.add('hide');
+    }
+    else{
+        radioMascDiv.classList.remove('hide');
+        radioFemDiv.classList.remove('hide');
     }
 }
 
 function disable(){
     nomeInp.setAttribute("disabled","");
-    fmInp.setAttribute("disabled","");
-    ffInp.setAttribute("disabled","");
+    mascInp.setAttribute("disabled","");
+    femInp.setAttribute("disabled","");
     emailInp.setAttribute("disabled","");
 }
 
 function enable(){
     nomeInp.removeAttribute("disabled");
-    fmInp.removeAttribute("disabled");
-    ffInp.removeAttribute("disabled");
+    mascInp.removeAttribute("disabled");
+    femInp.removeAttribute("disabled");
     emailInp.removeAttribute("disabled");
 }
--- resources/views/editarPerfil.blade.php
+++ resources/views/editarPerfil.blade.php
@@ -24,24 +24,24 @@
     <h1>MEU PERFIL</h1>
     <section>
         <div class="container-1" id="perfil">
-            <div class="box">
+            <div class="box" id="perfilArea">
                 <div class="change">
                     <form id="register">
                         <div class="row">
                             <div class="col-lg-12">
                                 <label>Nome</label> <br>
-                                <input disabled id="fnome" name="fnome" type="text" maxlength="50" required>
+                                <input disabled id="fnome" name="fnome" type="text" maxlength="50">
                             </div>
                         </div>
 
                         <div class="row">
                             <div class="col-lg-4">
                                 <label>Data de Nascimento</label> <br>
-                                <input disabled id="fnascimento" name="fnascimento" type="date" required>
+                                <input disabled id="fnascimento" name="fnascimento" type="date">
                             </div>
                             <div class="col-lg-4">
                                 <label>CPF</label> <br>
-                                <input disabled id="fcpf" name="fcpf" type="text" required maxlength="14"
+                                <input disabled id="fcpf" name="fcpf" type="text" maxlength="14"
                                     pattern="\d{3}\.\d{3}\.\d{3}-\d{2}">
                             </div>
                             <div class="col-lg-4">
@@ -66,7 +66,7 @@
                         <div class="row">
                             <div class="col-lg-8">
                                 <label>Email</label> <br>
-                                <input disabled id="femail" name="femail" type="email" maxlength="50" required>
+                                <input disabled id="femail" name="femail" type="email" maxlength="50">
                             </div>
                             <div class="col-lg-4" id="atribuiDiv">
                                 <label for="fatribui">Atribuição</label>
@@ -75,15 +75,15 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-lg-4" id="corenDiv" style="display: none;">
+                            <div class="col-lg-4" id="corenDiv">
                                 <!--Mostrar isso somente se for Enfermeiro/Chefe-->
                                 <label for="fcoren">Coren</label>
-                                <input disabled id="fcoren" name="fcoren" type="text" value="Coren do Usuario" required
+                                <input disabled id="fcoren" name="fcoren" type="text" value="Coren do Usuario"
                                     maxlength="9">
                             </div>
                             <div class="col-lg-4" id="edit_div">
                                 <br>
-                                <div id="psw_info_div" style="display: none;">
+                                <div id="psw_info_div">
                                     <button type="button" class="btn-white" name="psw_info" id="psw_info">Alterar
                                         senha</button>
                                 </div>
@@ -94,7 +94,7 @@
                                 <button type="button" class="btn-blue" name="edit_info" id="edit_info">Editar
                                     informações</button>
                             </div>
-                            <div class="col-lg" id="confirm_info_div" style="display: none;">
+                            <div class="col-lg hide" id="confirm_info_div">
                                 <button type="button" class="btn-blue" name="confirm_info"
                                     id="confirm_info">Salvar</button>
                                 <!--Alterar para Submit depois-->
@@ -103,30 +103,30 @@
                     </form>
                 </div>
             </div>
-            <div class="set-password">
-                <div id="psw" style="display: none">
+            <div class="set-password hide" id="pswArea">
+                <div>
                     <h3 class="password-title text-center">Alterar senha</h3>
                     <br> <br>
                     <form class="content-center">
                         <div align="center">
                             <input type="password" id="senha-atual" name="senha-atual"
-                                placeholder="insira a senha atual" required>
+                                placeholder="insira a senha atual">
                         </div>
                         <div align="center">
-                            <input type="password" id="senha" name="senha" placeholder="insira a nova senha" required>
+                            <input type="password" id="senha" name="senha" placeholder="insira a nova senha">
                         </div>
                         <div align="center">
                             <input type="password" id="confirmacao" name="confirmacao"
-                                placeholder="Confirme a nova senha" required>
+                                placeholder="Confirme a nova senha">
                         </div>
                         <br>
                         <div class="content-right">
                             <div class="row">
                                 <div class="col-lg">
-                                    <button class="btn-gray">Cancelar</button>
+                                    <button id="cancelar" type="button" class="btn-gray">Cancelar</button>
                                 </div>
                                 <div class="col-lg">
-                                    <button type="submit" class="btn-blue">Alterar senha</button>
+                                    <button id="alterarSenha" type="submit" class="btn-blue">Alterar senha</button>
                                 </div>
                             </div>
                         </div>
@@ -134,13 +134,7 @@
                 </div>
             </div>
         </div>
-
     </section>
-    <script>
-        document.forms['teste']['fcpf'].disabled = true
-        document.forms['teste']['fatribui'].disabled = true;
-
-    </script>
 </body>
 
 </html>
