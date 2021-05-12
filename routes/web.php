9b91766f3e6a6fe9f96edf3c6e6f98e8c8358713
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2021-05-12 08:13:12 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,7 @@ Route::get('/meuPerfil', [HomeController::class,'editPerfil'])->name('editarPerf
 
 Route::get('/menu', [AdminController::class,'menu'])->name('menu');
 Route::get('/log', [AdminController::class,'log'])->name('log');
+Route::get('/editarAtribuicao', [AdminController::class,'atribuicao'])->name('editarAtribuicao'); /* adicionado por João Vítor Santos */
 Route::post('/editarAtribuicao', [AdminController::class,'atribuicao'])->name('editarAtribuicao');
 Route::get('/editarPermissao', [AdminController::class,'permissao'])->name('editarPermissao');
 Route::get('/alterarPermissao', [AdminController::class,'alterarPermissao'])->name('alterarPermissao');
