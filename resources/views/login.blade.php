eb2921a1e58c17e12bd62ed9e805291aece43e7e
Estéfane <53922973+tefscs@users.noreply.github.com>
2021-05-12 17:35:20 -0300

    nome da aba

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,7 +8,7 @@
     <!-- Favicon -->
     <link href="{{ asset('img/favicon.png') }}" rel="icon">
 
-    <title>NOME DO NOSSO </title>
+    <title>Controle de medicamentos</title>
      
     <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
     <link href="{{ ('css/login-style.css') }}" rel="stylesheet"> 
