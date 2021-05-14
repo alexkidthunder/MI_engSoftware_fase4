e45b357902877d475a652779d42b6f37e9332b60
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-05-14 15:30:52 -0300

    Atualização Banco de dados
    Retirada de Responsáveis, tabela com CASCADE

--- BD/hospital_universitario.sql
+++ BD/hospital_universitario.sql
@@ -46,10 +46,6 @@ CREATE TABLE cid_prontuario (
   id_prontuario bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Cria tabela de Responsavel*/
-CREATE TABLE responsaveis(
-	CPF char(14) NOT NULL /* Chave estrangeira que faz referência ao responsável*/
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Cria tabela de enfermeiros*/
 CREATE TABLE enfermeiros (
@@ -173,7 +169,7 @@ ALTER TABLE cid_prontuario
   ADD KEY id_prontuario (id_prontuario);
 
 
-ALTER TABLE responsaveis
+ALTER TABLE usuarios
   ADD PRIMARY KEY CPF (CPF); /* Seleciona o campo CPF como chave primaria*/
   
 ALTER TABLE enfermeiros
@@ -211,8 +207,6 @@ ALTER TABLE prontuarios
   ADD KEY Id_leito (Id_leito), /* Seleciona o campo como chave */
   ADD KEY Cpfpaciente (Cpfpaciente); /* Seleciona o campo como chave */
 
-ALTER TABLE usuarios
-  ADD PRIMARY KEY (CPF); /* Seleciona o campo CPF como chave primaria*/
   
 ALTER TABLE pacientes
   ADD PRIMARY KEY (CPF); /* Seleciona o campo CPF como chave primaria*/
@@ -244,7 +238,7 @@ ALTER TABLE administradores
 
 ALTER TABLE agendamentos
   ADD CONSTRAINT agendamento_ibfk_1 FOREIGN KEY (ID_prontuario) REFERENCES prontuarios (ID), /*Cria chave estrangeira fazendo referencia a prontuario*/
-  ADD CONSTRAINT agendamento_ibfk_2 FOREIGN KEY (CPF_usuario) REFERENCES responsaveis (CPF), /*Cria chave estrangeira fazendo referencia a responsavel*/
+  ADD CONSTRAINT agendamento_ibfk_2 FOREIGN KEY (CPF_usuario) REFERENCES usuarios (CPF), /*Cria chave estrangeira fazendo referencia a usuários*/
   ADD CONSTRAINT agendamento_ibfk_3 FOREIGN KEY (Cod_medicamento) REFERENCES medicamentos (Codigo); /*Cria chave estrangeira fazendo referencia a medicamento*/
 
 ALTER TABLE agendamento_prontuario
@@ -255,21 +249,18 @@ ALTER TABLE cid_prontuario
   ADD CONSTRAINT cid_prontuario_ibfk_1 FOREIGN KEY (id_CID) REFERENCES cid (Id) ON DELETE CASCADE,/*Cria chave estrangeira fazendo referencia ao CID*/
   ADD CONSTRAINT cid_prontuario_ibfk_2 FOREIGN KEY (id_prontuario) REFERENCES prontuarios (ID);/*Cria chave estrangeira fazendo referencia a prontuario*/
 
-ALTER TABLE responsaveis
-  ADD CONSTRAINT responsaveis_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF);/*Cria chave estrangeira fazendo referencia a Usuario*/
-
 ALTER TABLE enfermeiros
-  ADD CONSTRAINT enfermeiros_ibfk_1 FOREIGN KEY (CPF) REFERENCES responsaveis (CPF);/*Cria chave estrangeira fazendo referencia a Usuario*/
+  ADD CONSTRAINT enfermeiros_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF)ON DELETE CASCADE;/*Cria chave estrangeira fazendo referencia a Usuario*/
 
 ALTER TABLE enfermeiros_chefes
-  ADD CONSTRAINT enfermeiros_chefes_ibfk_1 FOREIGN KEY (CPF) REFERENCES responsaveis (CPF); /*Cria chave estrangeira fazendo referencia a Usuario*/
+  ADD CONSTRAINT enfermeiros_chefes_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF)ON DELETE CASCADE; /*Cria chave estrangeira fazendo referencia a Usuario*/
 
 ALTER TABLE estagiarios
-  ADD CONSTRAINT estagiarios_ibfk_1 FOREIGN KEY (CPF) REFERENCES responsaveis (CPF); /*Cria chave estrangeira fazendo referencia a Usuario*/
+  ADD CONSTRAINT estagiarios_ibfk_1 FOREIGN KEY (CPF) REFERENCES usuarios (CPF)ON DELETE CASCADE; /*Cria chave estrangeira fazendo referencia a Usuario*/
 
 ALTER TABLE ocorrencias
   ADD CONSTRAINT ocorrencia_ibfk_1 FOREIGN KEY (ID_prontuario) REFERENCES prontuarios (ID), /*Cria chave estrangeira fazendo referencia a prontuario*/
-  ADD CONSTRAINT ocorrencia_ibfk_2 FOREIGN KEY (CPF) REFERENCES responsaveis (CPF); /*Cria chave estrangeira fazendo referencia a Usuario*/
+  ADD CONSTRAINT ocorrencia_ibfk_2 FOREIGN KEY (CPF) REFERENCES usuarios (CPF); /*Cria chave estrangeira fazendo referencia a Usuario*/
 
 ALTER TABLE permissao_cargo
   ADD CONSTRAINT permissao_cargo_ibfk_1 FOREIGN KEY (cargo_id) REFERENCES cargo (id) ON DELETE CASCADE,/*Cria chave estrangeira fazendo referencia a cargo*/
