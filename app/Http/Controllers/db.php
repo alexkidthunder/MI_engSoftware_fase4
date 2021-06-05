83fcf086410b16f85ea99d9b6921c1e2c3ce70e3
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-05 19:28:17 -0300

    Coneção com .env

--- app/Http/Controllers/db.php
+++ app/Http/Controllers/db.php
@@ -1,4 +1,4 @@
 <?php
-    $connect = mysqli_connect("localhost","root","") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
-    $db = mysqli_select_db($connect,"hospital_universitario") or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
+    $connect = mysqli_connect("$_ENV['DB_HOST']","$_ENV['DB_USERNAME']","$_ENV['DB_PASSWORD']") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
+    $db = mysqli_select_db($connect,"$_ENV['DB_DATABASE']") or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
 ?>
