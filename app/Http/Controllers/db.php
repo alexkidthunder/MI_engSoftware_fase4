564af73c73866193f6619827b9eacc6c6a0cedeb
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-05 20:21:39 -0300

    Coneção com .env

--- app/Http/Controllers/db.php
+++ app/Http/Controllers/db.php
@@ -1,12 +1,4 @@
 <?php
-$host = env('DB_HOST', 'null');
-$port= env('DB_PORT', 'null');
-$dbname= env('DB_DATABASE', 'null');
-$user= env('DB_USERNAME', 'null');
-$password= env('DB_PASSWORD', 'null');
-
-$connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
-
-$db = pg_connect($connection_string) or die("Falha ao conectar o servidor"); /*conectar ao servidor */
+    $connect = mysqli_connect("localhost","root","") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
+    $db = mysqli_select_db($connect,"hospital_universitario") or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
 ?>
-
