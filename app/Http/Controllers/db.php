ecd86a27e6b7a5c8bbd3e7c1112a5ed9269367a0
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-05 22:08:14 -0300

    Coneção com .env

--- app/Http/Controllers/db.php
+++ app/Http/Controllers/db.php
@@ -1,10 +1,4 @@
 <?php
-    $dbconfig = parse_ini_file(".env");
-    $host = $dbconfig["DB_HOST"];
-    $user = $dbconfig["DB_USERNAME"];
-    $password = $dbconfig["DB_PASSWORD"];
-    $database = $dbconfig["DB_DATABASE"];
-
-    $connect = mysqli_connect($host,$user,$password) or die("Falha ao conectar o servidor"); /*conectar ao servidor */
-    $db = mysqli_select_db($connect,$database) or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
+    $connect = mysqli_connect(env('DB_HOST', 'null'),env('DB_USERNAME', 'null'),env('DB_PASSWORD', 'null')) or die("Falha ao conectar o servidor"); /*conectar ao servidor */
+    $db = mysqli_select_db($connect,env('DB_DATABASE', 'null')) or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
 ?>
