0e94edd1f2e63489d67cef818c07de283f27fdbf
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-05 22:00:31 -0300

    Coneção com .env

--- app/Http/Controllers/db.php
+++ app/Http/Controllers/db.php
@@ -1,4 +1,4 @@
 <?php
-    $connect = mysqli_connect("us-cdbr-east-04.cleardb.com","b4e2cf5fbf24e6","56ccb943") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
-    $db = mysqli_select_db($connect,"heroku_30c49258aaa92a9") or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
+    $connect = mysqli_connect("$_ENV['DB_HOST']","$_ENV['DB_USERNAME']","$_ENV['DB_PASSWORD']") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
+    $db = mysqli_select_db($connect,"$_ENV['DB_DATABASE']") or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
 ?>
