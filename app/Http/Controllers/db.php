225e71adfbc467be092b8b2c33ad19e103fdb928
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-06 23:10:18 -0300

    Pegando atualização antigas
    Pras configurações antes do commit de Aurélio

--- app/Http/Controllers/db.php
+++ app/Http/Controllers/db.php
@@ -1,4 +1,4 @@
 <?php
-    $connect = mysqli_connect("us-cdbr-east-04.cleardb.com","b4e2cf5fbf24e6","56ccb943git") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
-    $db = mysqli_select_db($connect,"heroku_30c49258aaa92a9") or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
+    $connect = mysqli_connect(env('DB_HOST', 'null'),env('DB_USERNAME', 'null'),env('DB_PASSWORD', 'null')) or die("Falha ao conectar o servidor"); /*conectar ao servidor */
+    $db = mysqli_select_db($connect,env('DB_DATABASE', 'null')) or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
 ?>
