d06395236bcb3579cdcc41b57a2c65d0def248a8
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-06 22:53:13 -0300

    teste2

--- app/Http/Controllers/db.php
+++ app/Http/Controllers/db.php
@@ -1,4 +1,4 @@
 <?php
-    $connect = mysqli_connect("us-cdbr-east-04.cleardb.com","b4e2cf5fbf24e6","56ccb943") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
+    $connect = mysqli_connect("us-cdbr-east-04.cleardb.com","b4e2cf5fbf24e6","56ccb943git") or die("Falha ao conectar o servidor"); /*conectar ao servidor */
     $db = mysqli_select_db($connect,"heroku_30c49258aaa92a9") or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
 ?>
