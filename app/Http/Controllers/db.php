86b50b96232f630c6de5fc10372d23f2fc1c2828
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-08 18:49:01 -0300

    testando

--- app/Http/Controllers/db.php
+++ app/Http/Controllers/db.php
@@ -1,4 +1,6 @@
 <?php
+
     $connect = mysqli_connect(env('DB_HOST', 'null'),env('DB_USERNAME', 'null'),env('DB_PASSWORD', 'null')) or die("Falha ao conectar o servidor"); /*conectar ao servidor */
     $db = mysqli_select_db($connect,env('DB_DATABASE', 'null')) or die("Falha ao selecionar o banco de dados"); /* Selecionando o banco de dados */
+
 ?>
