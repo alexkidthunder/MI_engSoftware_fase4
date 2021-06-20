664e7a49143681c8664e8293b898df4301b22409
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-06-20 13:19:42 -0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        ChecarAgendamento::class,
-        AgendaBackup::class,
+        //
     ];
 
     /**
